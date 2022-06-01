f86d7c3ade07dc701abffb3ccde890384f432dba
GytisKlnt <gytis.kalanta@gmail.com>
2022-06-01 19:48:35 +0300

    Added section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
+import Section from "./components/Section/Section";
 import Button from "./components/Button/Button";
 
 const App = () => {
   return (
     <>
       <Hero title="New York" subtitle="Find out everything about NY here!" />
-      <Button type="button" color="bold">
-        Button!!
-      </Button>
-      <Button type="button">Button!!</Button>
+      <Section>
+        <Button type="button" color="bold">
+          Button!!
+        </Button>
+        <Button type="button">Button!!</Button>
+      </Section>
     </>
   );
 };
